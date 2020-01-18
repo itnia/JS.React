8db9ae36ccf2db6a5fdae70c26637596d7b6a03e
itnia <itnia.aurey@outlook.com>
2020-01-18 12:45:31 +0300

    git -commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+//import { Router } from 'react-router';
+//import {} from  '';
 import './style.css';
 
 //-------------------------------------------  cd my-app     npm start
 
+const News = () => {
+  return (
+    <>
+      <p>Title</p>
+      <p>text</p>
+    </>
+  )
+}
 
 
-
+class Saite extends React.Component {
+  render () {
+    return (
+      <>
+        <h1>NameSaite</h1>
+        <News/>
+      </>
+    )
+  }
+}
 //-------------------------------------------
   
 ReactDOM.render(
-  <BrowserRouter>
-    
-  </BrowserRouter>,
+  <Saite/>,
   document.getElementById('root')
 );
