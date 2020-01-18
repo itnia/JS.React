0d32326629d5a68febb3b36810945324ed14c8fd
itnia <itnia.aurey@outlook.com>
2020-01-18 12:02:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './style.css';
+
+//-------------------------------------------  cd my-app     npm start
+
+
+
+
+//-------------------------------------------
+  
+ReactDOM.render(
+  <BrowserRouter>
+    
+  </BrowserRouter>,
+  document.getElementById('root')
+);
